9f3ada6fbf0513f6d940655a63cc9ad86fa6c26c
noelpei0461 <31324311+noelpei0461@users.noreply.github.com>
2017-09-13 04:43:43 +0800

    Update inclass5.m

--- inclass5.m
+++ inclass5.m
@@ -47,3 +47,21 @@ end
 %is, returns the complementary base pair for each base, read from end
 %to beginning. (e.g. the reverse compliment of ATGC is GCAT). Do not use the
 %builtin MATLAB function for this. 
+
+function cc=reverse(x)
+rx='';
+len=length(x);
+for ii=1:len
+    if x(ii)=='A'
+        rx(ii)='T'
+    elseif x(ii)=='T'
+        rx(ii)='A'
+    elseif x(ii)=='G'
+        rx(ii)='C'
+    elseif x(ii)=='C'
+        rx(ii)='G'
+    end
+end
+cc=flip(rx);
+end
+
